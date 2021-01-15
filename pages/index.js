8acb74b2ea561b35d18bb65ca960e2a9eb2abb4e
Matias Benedetto <matias.benedetto@gmail.com>
2021-01-15 12:10:04 -0300

    favicon

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Hi 👋, my name is Matias Benedetto</title>
         <meta property="og:title" content="Hi 👋, my name is Matias Benedetto" key="title" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍💻</text></svg>"></link>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
